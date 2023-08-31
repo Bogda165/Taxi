1045625d96dcb19227adea87c04a9467501f5619
Bogda165 <koval.b@icloud.com>
2023-08-31 09:08:13 +0200

    Initial Commit

---
+++ Taxi_company/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Taxi_company
+//
+//  Created by Богдан Коваль on 31.08.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
